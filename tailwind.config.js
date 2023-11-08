ae58da0b7ad23e71beaae99f4be558790bd64913
Brad Liu <bd5105625@gmail.com>
2023-11-08 13:25:31 -0600

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
       // "./src/*.{js,jsx,ts,tsx}",
       "./src/landing/registration.js",
+      "./src/landing/component/loginForm.js",
       "./src/**/*.{js,jsx,ts,tsx}",
       'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
