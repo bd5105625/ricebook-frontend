ac3c34a8688eb2c79f21d95a80775cf345f0e0bc
Brad Liu <bd5105625@gmail.com>
2023-11-08 12:07:00 -0600

    change url

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://finalwebbrad.herokuapp.com"
 // export const BASE_URL = " http://comp531hw6.herokuapp.com"
-export const BASE_URL = 'https://ricebook-backend-65d6f66f731f.herokuapp.com/'
+export const BASE_URL = 'https://ricebook-backend-65d6f66f731f.herokuapp.com'
 // export const BASE_URL = "http://localhost:4000"
